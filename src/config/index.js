c8a180f3095be350cc62e66fe4163d82ab2e21b2
ijul <ijuldz@gmail.com>
2018-01-01 11:06:05 +0700

    oke

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,4 @@
 export default {
-  // url: 'http://lunario.id:8000',
-  // url: 'http://inspiralocal.id:8001',
-  url: '103.89.3.246:8021',
-  auth: 'lunario-app-rn',
   color: {
     primary: '#2196F3',
     primaryDarker: '#1565C0',
@@ -11,5 +7,4 @@ export default {
     red: '#F44336',
     accentYellow: '#FFAB00',
   },
-  youtubeApiKey: 'AIzaSyBv3t7GpgdU6QCYYa07NW4avrK7QiKfnYg',
 };

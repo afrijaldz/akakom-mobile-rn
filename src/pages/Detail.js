6c584408e0c41fd2f177edf90021c97c5168bc81
ijul <ijuldz@gmail.com>
2018-01-01 16:55:49 +0700

    detail beranda

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Detail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: '',
+    }
+  }
+
+  componentDidMount() {    
+    this.setState({ data: this.props.navigation.state.params.data });
+    console.log(this.state.data);
+  }
+
   render() {
-    console.log(this.props.navigation);
+    const { title, more, content, date, image } = this.state.data;
 
     return (
-      <View style={{ backgroundColor: '#fff', flex: 1 }}>
-        <Text>
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={{ marginBottom: 10 }}>
+          <Card
+            title={title}
+            image={{ uri: image }}>
+            <View>
+              <Text>
+                {more}
+              </Text>
+            </View>
+          </Card>
+        </View>
+      </ScrollView>
     );
   }
 }
